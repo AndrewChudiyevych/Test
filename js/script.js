c954234bd9b4e77e738e52d1b6e43aad995a7966
Andrew <qwertyuiop1234567966@gmail.com>
2020-10-02 11:30:45 +0300

    movies commit

--- js/script.js
+++ js/script.js
@@ -109,7 +109,7 @@ alert(`Hello,${user}`);*/
 
 //ОПЕРАТОРИ
 
-console.log('arr' + " -object");
+/*console.log('arr' + " -object");
 
 console.log(4 + " -object");
 
@@ -121,4 +121,24 @@ let incr = 10,
 //++incr;
 //--decr;
 
-console.log(incr++ + "/" + decr++);
+console.log(incr++ + "/" + decr++);*/
+
+const numberOfFilms = +prompt('Скільки фільмів ви уже подивилися?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один із останніх переглянутих фільмів?', ''),
+    b = prompt('На скільки оціните його?', ''),
+    c = prompt('Один із останніх переглянутих фільмів?', ''),
+    d = prompt('На скільки оціните його?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
