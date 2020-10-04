1defcf323eb7f98843ff72ca5a078530766502b1
Andrew <qwertyuiop1234567966@gmail.com>
2020-10-04 12:36:47 +0300

    a lot

--- js/script.js
+++ js/script.js
@@ -123,7 +123,9 @@ let incr = 10,
 
 console.log(incr++ + "/" + decr++);*/
 
-const numberOfFilms = +prompt('Скільки фільмів ви уже подивилися?', '');
+
+//ПРАКТИКА - 1
+/*const numberOfFilms = +prompt('Скільки фільмів ви уже подивилися?', '');
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -141,4 +143,353 @@ const a = prompt('Один із останніх переглянутих філ
 personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+console.log(personalMovieDB);*/
+
+//УСЛОВІЯ
+
+/*if (4 == 4) {
+    console.log("4 равно 4");
+} else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num < 49) {
+    console.log('Error');
+} else if (num > 100) {
+    console.log('Too much');
+} else {
+    console.log('Ok!');
+}
+
+//ТЕРНАРНИЙ ОПЕРАТОР
+
+(num === 50) ? console.log('Ok!'): console.log('Error');
+
+
+switch (num) {
+    case 49:
+        console.log('49');
+        break;
+    case 50:
+        console.log('50');
+        break;
+    case 90:
+        console.log('90');
+        break;
+    default:
+        console.log('DEFAULT');
+        break;
+}*/
+
+
+//ЦИКЛИ
+
+/*let num = 50;
+
+while (num < 55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num <= 55);
+
+for (let i = 1; i < 8; i++) {
+    console.log(i);
+}*/
+
+//СТРІЛКОВІ ФУНКЦІЇ, ФУНКЦІЇ
+
+/*let num = 20;
+
+function showFirstMessage(text) {
+    console.log(text);
+    console.log(num);
+}
+
+showFirstMessage('Hello world!');
+console.log(num);
+
+function calk(a, b) {
+    return a + b;
+}
+
+console.log(calk(10, 5));
+
+function retNum() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = retNum();
+
+console.log(anotherNum);
+
+console.log(retNum());
+
+const logger = function() {
+    console.log('Hello');
+};
+
+logger();
+
+const calkulator = (a, b) => {
+    console.log('Done!');
+    return a + b;
+};*/
+
+//МЕТОДИ І ВЛАСТИВОСТІ ЧИСЕЛ
+
+/*const str = 'Text';
+
+console.log(str.length);
+
+const arr = [1, 2, 3];
+
+console.log(arr.length);
+
+console.log(str[2]);
+
+console.log(str.toUpperCase());
+console.log(str.toLowerCase());
+
+let fruit = 'Some fruit';
+
+console.log(fruit.indexOf('fruit'));
+
+const logg = 'Hello world!';
+
+console.log(logg.slice(6, 11));
+
+console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 11));
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const tests = '12.2px';
+
+console.log(parseInt(tests));
+
+console.log(parseFloat(tests));
+
+
+console.log(str);*/
+
+//CALLBACK ФУНКЦІЇ
+
+/*function first() {
+    //Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
+}
+
+function second() {
+    console.log(2);
+}
+
+first();
+second();
+
+function learnJS(lang, callback) {
+    console.log(`I learn: ${lang}`);
+    callback();
+}
+
+function done() {
+    console.log('I passed this lesson');
+}
+
+learnJS('JavaScript', done);*/
+
+//ОБЪЕКТ
+
+/*const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        background: 'red'
+    },
+    makeTest: function() {
+        console.log('Test');
+    }
+};
+
+options.makeTest();
+
+const {border, bg} = options.colors;
+
+console.log(border);
+
+console.log(Object.keys(options).length);
+
+console.log(options.name);
+
+//delete options.name;
+
+let counter = 0;
+
+for (let key in options) {
+    if (typeof(options[key]) === 'object') {
+        for (let i in options[key]) {
+            console.log(`Options: ${i}, have meaninng: ${options[key][i]}`);
+            counter++;
+        }
+    } else {
+        console.log(`Options: ${key}, have meaninng: ${options[key]}`);
+        counter++;
+    }
+}
+
+console.log(counter);*/
+
+
+//МАСИВИ І ПСЕВДОМАСИВИ
+
+/*const arr = [1, 8, 3, 5, 2];
+
+arr.sort(compareNum);
+
+console.log(arr);
+
+function compareNum(a, b) {
+    return a - b;
+}
+
+//console.log(arr.length);
+
+//arr[99] = 0;
+
+//console.log(arr);
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item}: in array ${arr}`);
+});
+
+//arr.pop();
+//arr.push(10);
+
+//console.log(arr);
+
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+}
+
+for (let value of arr) {
+    console.log(value);
+}
+
+
+const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products);
+console.log(products.join('; '));*/
+
+//Передача по силці
+
+let a = 7;
+
+let b = a;
+
+b = b + 5;
+
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+
+const copy = obj;
+
+copy.a = 10;
+
+console.log(copy);
+
+console.log(obj);
+
+function copyObject(mainObj) {
+    let objCopy = {};
+    for (let key in mainObj) {
+        objCopy[key] = mainObj;
+    }
+
+    return objCopy;
+}
+
+const numbers = {
+    a: 2,
+    b: 3,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copyObject(numbers);
+
+newNumbers.a = 10;
+newNumbers.c.x = 10;
+
+console.log(newNumbers);
+
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+
+//console.log(add);
+//console.log(clone);
+
+const oldArray = ['a', 'b', 'c'];
+const newArray = oldArray.slice();
+
+newArray[1] = 'adadada';
+console.log(oldArray);
+console.log(newArray);
+
+const video = ['you tube', 'vimeo', 'rutube'],
+    blog = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blog, 'vk', 'facebook'];
+
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+}
+
+const num = [2, 5, 7];
+
+log(...num);
+
+const array = ["a", "b"];
+
+const newAray = [...array];
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newObj = {...q };
